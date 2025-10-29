7511c125e434f9e38ff7f8e5ed0b7adab115bc26
suporteptrefurb-star <suporte.ptrefurb@gmail.com>
2025-10-29 16:03:07 +0000

    Add files via upload

--- servidor_zebra.py
+++ servidor_zebra.py
@@ -1,18 +1,21 @@
-import requests
+import socket
 from flask import Flask, request
 
 app = Flask(__name__)
 
-LOCAL_SERVER = "http://SEU_IP_LOCAL:5000/imprimir"
+ZEBRA_IP = "192.168.1.103"  # IP da impressora Zebra na tua rede
+ZEBRA_PORT = 9100
 
 @app.route('/imprimir', methods=['POST'])
 def imprimir():
     zpl = request.get_data(as_text=True)
     try:
-        response = requests.post(LOCAL_SERVER, data=zpl)
-        return response.text, response.status_code
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as zebra:
+            zebra.connect((ZEBRA_IP, ZEBRA_PORT))
+            zebra.sendall(zpl.encode('utf-8'))
+        return "Etiqueta enviada com sucesso!", 200
     except Exception as e:
-        return f"Erro ao redirecionar: {str(e)}", 500
+        return f"Erro ao enviar etiqueta: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
